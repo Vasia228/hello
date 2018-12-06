0a121c1660f67e62364ee054bce22d28de8845b8
Vasia228 <43805731+Vasia228@users.noreply.github.com>
2018-12-06 14:07:43 +0300

    Add files via upload

--- Сдать/brekets.py
+++ Сдать/brekets.py
@@ -1,4 +1,7 @@
+#origin="????????????????????????????????????????"
+#origin="??????????????????????????????"
 origin="????????????????????"
+#origin="????"
 print(origin)
 
 if len(origin)%2!=0 or origin[0]==")" or origin[-1]=="(":
@@ -7,37 +10,37 @@ if len(origin)%2!=0 or origin[0]==")" or origin[-1]=="(":
 
 origin="("+origin[1:-1]+")"
 
-buf=[""]
 counters=[0]
 remaining=len(origin)
 deadlist=[]
+tmp=[]
 
 for elem in origin:
+    print(remaining)
     remaining-=1
 
     if elem == "(" or elem==")":
         data=-1
         if elem=="(":
             data=1
-        for index in range(len(buf)):
-            buf[index]+=(elem)
+        for index in range(len(counters)):
             counters[index]+=data
             if counters[index] < 0 or counters[index]>remaining:
                 deadlist.append(index)
+
     else:
-        for index in range(len(buf)):
+        for index in range(len(counters)):
             if (counters[index]-1)>=0:
-                buf.append(buf[index]+")")
                 counters.append(counters[index]-1)
             if counters[index]+1<=remaining:
-                buf[index]+="("
                 counters[index]+=1
             else:
                 deadlist.append(index)
+    
     for e in deadlist:
-        buf[e]=buf.pop()
         counters[e]=counters.pop()
     deadlist.clear()
-print(len(buf))
+
+print("Ans:",len(counters))
 
 
